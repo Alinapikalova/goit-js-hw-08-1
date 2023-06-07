511ab1745d0b11c3341a6c78a8da0aaa34b3a583
Alinapikalova <alina.pikalova@gmail.com>
2023-06-07 10:36:52 +0200

    add some js code for js-08 (2)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+//on
+player.on('eventName', function (data) {
+  // data is an object containing properties specific to that event
+});
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
 
